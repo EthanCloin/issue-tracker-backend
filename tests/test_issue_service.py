7e6edecf2d1bf89546b554765b0e5e4bcb08dfb0
Ethan Cloin <psnethan@gmail.com>
2022-11-25 10:50:10 -0500

    add test for get single issue

--- tests/test_issue_service.py
+++ tests/test_issue_service.py
@@ -1,7 +1,10 @@
 from fastapi.testclient import TestClient
-from app.api.v1 import issue_service
-from app.schema import issue as issue_schema
 
+from app.api.v1 import issue_service
+
+# TODO: put test data into a single central location.
+#  the crud file can pull from
+#  there to get the initial rows as well.
 client = TestClient(issue_service.app)
 
 
@@ -9,6 +12,18 @@ def test_get_all_returns_200():
     response = client.get("/issues/")
     assert response.status_code == 200
     assert len(response.json()) == 5
+
+
+def test_get_one_returns_200():
+    response = client.get("/issues/1")
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": 1,
+        "title": "1 Problem",
+        "description": "really does it matter",
+        "assignee": "mgmt",
+        "status": "open",
+    }
 
 
 # def test_create_issue_success_with_full_issue():
