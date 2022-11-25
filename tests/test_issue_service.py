9142c1bf6823a53d857e988a68f7d8bcb9304f2b
Ethan Cloin <psnethan@gmail.com>
2022-11-25 10:45:46 -0500

    add test for getall endpoint

--- tests/test_issue_service.py
+++ tests/test_issue_service.py
@@ -1,20 +1,27 @@
 from fastapi.testclient import TestClient
-from app.api.v1 import issue_service, dependencies
+from app.api.v1 import issue_service
 from app.schema import issue as issue_schema
 
 client = TestClient(issue_service.app)
 
-def test_create_issue_success_with_full_issue():
-    new_issue = issue_schema.IssueCreate(
-        title="My Test Title",
-        description="""
-    My Test Description is longer than My TestTitle and includes
-    
-    Multiple 
-    weird whitespace    characters""",
-        assignee="My Test Assignee",
-    )
-    body = new_issue.dict()
-    response = client.post("/issues/", json=body)
-    print("testing")
-    print(response)
+
+def test_get_all_returns_200():
+    response = client.get("/issues/")
+    assert response.status_code == 200
+    assert len(response.json()) == 5
+
+
+# def test_create_issue_success_with_full_issue():
+#     new_issue = issue_schema.IssueCreate(
+#         title="My Test Title",
+#         description="""
+#     My Test Description is longer than My TestTitle and includes
+#
+#     Multiple
+#     weird whitespace    characters""",
+#         assignee="My Test Assignee",
+#     )
+#     body = new_issue.dict()
+#     response = client.post("/issues/", json=body)
+#     print("testing")
+#     print(response)
