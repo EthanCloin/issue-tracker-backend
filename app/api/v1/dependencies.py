21ccac3c38081b925c4c456576c075e699bafede
Ethan Cloin <psnethan@gmail.com>
2022-11-27 16:08:30 -0500

    update get_db dependency to use get_session_for

--- app/api/v1/dependencies.py
+++ app/api/v1/dependencies.py
@@ -1,13 +1,14 @@
 from typing import Generator
 
-from app.database.connector import LocalSession
+from app.database.connector import get_session_for, DBEnv
 
 
 def get_db() -> Generator:
     """call this to get a Session object for use for execution of
     db requests"""
+    maker = get_session_for(DBEnv.STAGING)
     try:
-        db = LocalSession()
+        db = maker()
         yield db
     finally:
         db.close()
